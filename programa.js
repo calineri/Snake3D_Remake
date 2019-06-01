0f466b7e67315ff94bf0c34116fca4c302903fd8
CELSO ALINERI <celso.alineri@senacsp.edu.br>
2019-05-31 22:16:46 -0300

    adcionada novas formas geometricas

--- programa.js
+++ programa.js
@@ -16,23 +16,33 @@ let frame = 0;
 let cobra = [];
 let maca;
 let cont = 2;
+let parede = [];
+let controle = false;
 
 function main(){
     // 1 - SETUP DA CENA / CAMERA E RENDERER
     setup();
 
-    // 2.0 - CRIAR CUBO 
+    // 2.0 - CRIAR COBRA 
     cobra[0] = getSnakeData();
     cobra[0].position.x = -1;
     cobra[0].material.emissive.setRGB(0, 1, 0);
+    //cobra[0].rotateZ(Math.PI/2);
+    cobra[0].rotation.z = Math.PI/2;
     cobra[1] = getSnakeData();
     cobra[1].position.x = -1.5;
+    //cobra[1].rotateZ(Math.PI/2);
+    cobra[1].rotation.z = Math.PI/2;
     cobra[2] = getSnakeData();
     cobra[2].position.x = -2.0;
+    //cobra[2].rotateZ(Math.PI/2);
+    cobra[2].rotation.z = Math.PI/2;
 
     maca = getAppleData();
 
     criaMaca();
+
+    desenhaCenario();
 
     // 2.1 - ADICIONAR À CENA
     scene.add( cobra[0] );
@@ -51,9 +61,7 @@ function main(){
 }
 
 function animate() {
-    //cube.rotateY(Math.PI/60);
-    //cube.position.x += 0.01;
-
+    
     if (iniciou){
         frame++
         if(frame % 10 === 0){
@@ -61,8 +69,7 @@ function animate() {
             movimentaCobra();
 
             
-            //if(colisaoProprioCorpo() || colisaoCenario()){
-            if(colisaoProprioCorpo()){
+            if(colisaoProprioCorpo() || colisaoCenario()){
                 gameOver = true;
                 iniciou = false;
                 console.log("Voce Perdeu!");
@@ -75,6 +82,8 @@ function animate() {
                 cobra[cont].position.x = cobra[0].position.x;
                 cobra[cont].position.y = cobra[0].position.y;
                 cobra[cont].position.z = cobra[0].position.z;
+                //cobra[cont].rotateZ(Math.PI/2);
+                cobra[cont].rotation.z = Math.PI/2;
                 scene.add( cobra[cont] );
 
                 criaMaca();
@@ -92,27 +101,47 @@ function animate() {
 
     }
 
+    
     renderer.render( scene, camera );
-    window.requestAnimationFrame( animate );
+    if(!gameOver){
+        window.requestAnimationFrame( animate );
+    }
 }
 
 function movimentaCobra(){
+
+
     for(let i = cobra.length-1; i>0; i--){
+        let x = cobra[i-1].position.x - cobra[i].position.x;
+        let z = cobra[i-1].position.z - cobra[i].position.z;
+        let a = Math.atan2(z, -x);
         cobra[i].position.x = cobra[i-1].position.x;
         cobra[i].position.z = cobra[i-1].position.z;
+
+        cobra[i].rotation.y = a;
+
+        
+
+
     }
+    let x = cobra[0].position.x + hor - cobra[0].position.x;
+    let z = cobra[0].position.z + ver - cobra[0].position.z;
+    let a = Math.atan2(z, -x);
+    cobra[0].rotation.y = a;
 
     cobra[0].position.x += hor;
     cobra[0].position.z += ver;
-
+    controle=false;
     return;
 }
 
 function criaMaca(){
     while(colisaoCorpoMaca()){
-        maca.position.x = (Math.floor(Math.random() * 14 + 1)) -7;
-        maca.position.z = (Math.floor(Math.random() * 14 + 1)) -7;
+        maca.position.x = (Math.floor(Math.random() * 10 + 1)) -5;
+        maca.position.z = (Math.floor(Math.random() * 10 + 1)) -5;
     }
+    console.log("Posicao X: " + maca.position.x);
+    console.log("Posicao Z: " + maca.position.z);
     return;
 }
 
@@ -148,15 +177,14 @@ function createLights(){
     scene.add( ambiente );
 
     let point = new THREE.PointLight( 0xffffff, 1, 100 );
-    point.position.set( 50, 50, 50 );
+    point.position.set( 0, 5, 5 );
     scene.add( point );
 }
 
 function getSnakeData(){
-    let geometry = new THREE.BoxGeometry( 0.5, 0.5, 0.5 );
-    let material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
+    let geometry = new THREE.CylinderGeometry( 0.3, 0.3, 0.5 , 20.0, 20.0);
+    let material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
     let cube = new THREE.Mesh( geometry, material );
-
     return cube;
 }
 
@@ -168,9 +196,17 @@ function getAppleData(){
     return cube;
 }
 
+function getWallData(){
+    //let geometry = new THREE.BoxGeometry( 0.5, 0.5, 0.5 );
+    let geometry = new THREE.ConeGeometry( 0.5, 1.5, 0.5 );
+    let material = new THREE.MeshLambertMaterial( { color: 0x0000ff } );
+    let cone = new THREE.Mesh( geometry, material );
+    return cone;
+}
+
 function setup(){
     aspect = window.innerWidth / window.innerHeight;
-    fovy = 90;
+    fovy = 80;
     near = 0.001;
     far = 1000;
 
@@ -190,8 +226,8 @@ function resize(){
 }
 
 function posicionarCamera(){
-    camera.position.z = 10;
-    camera.position.y = 9;
+    camera.position.z = 7;
+    camera.position.y = 7;
     camera.lookAt(0, 0, 0);
 }
 
@@ -199,32 +235,129 @@ function keyDown(evt){
     if(!gameOver){
         if(evt.key === "ArrowDown"){ 
             if (!blockver){
-                ver = .5, hor = 0.0, blockver = true, iniciou=true;
+                ver = .5, hor = 0.0, blockver = true, iniciou=true, controle=true;
             }
             return;
         }
         
         if(evt.key === "ArrowUp"){
             if (!blockver){
-                ver = -.5, hor = 0.0, blockver = true, iniciou=true;
+                ver = -.5, hor = 0.0, blockver = true, iniciou=true, controle=true;
             }
             return;
         }
 
         if(evt.key === "ArrowLeft"){
             if (!blockhor){
-                hor = -.5, ver = 0.0, blockhor = true;
+                hor = -.5, ver = 0.0, blockhor = true, controle=true;
             }
             return;
         }
 
         if(evt.key === "ArrowRight"){
             if (!blockhor){
-                hor = .5, ver = 0.0, blockhor = true, iniciou=true;
+                hor = .5, ver = 0.0, blockhor = true, iniciou=true, controle=true;
             }
             return;
         }
     }
+    
+    if(evt.key === "a"){
+        gameOver = true;
+    }
+
+    if(evt.key === "b" && gameOver){
+        gameOver = false;
+        animate();
+    }
+}
+
+function desenhaCenario(){
+    let cont = 0;
+    // Desenha borda superior da tela
+    for(let i=-6; i <= 6; i++){
+        parede[cont] = getWallData();
+        parede[cont].position.x = i;
+        parede[cont].position.z = -6;
+        scene.add(parede[cont]);
+        cont++;
+    }
+
+    // Desenha borda inferior da tela
+    for(let i=-6; i <= 6; i++){
+        parede[cont] = getWallData();
+        parede[cont].position.x = i;
+        parede[cont].position.z = 6;
+        scene.add(parede[cont]);
+        cont++;
+    }
+
+    // Desenha borda esquerda da tela
+    for(let i=-6; i <= 6; i++){
+        parede[cont] = getWallData();
+        parede[cont].position.x = -6;
+        parede[cont].position.z = i;
+        scene.add(parede[cont]);
+        cont++;
+    }   
+
+    // Desenha borda direita da tela
+    for(let i=-6; i <= 6; i++){
+        parede[cont] = getWallData();
+        parede[cont].position.x = 6;
+        parede[cont].position.z = i;
+        scene.add(parede[cont]);
+        cont++;
+    }
+
+    /*
+    // Desenha linha horizontal
+    for(let i=9; i>=-9; i--){
+        linha[0] = -7;
+        linha[2] = i;
+        modelLinha = mat4.fromTranslation([],linha);
+        gl.uniformMatrix4fv(modelUniform, false, modelLinha);
+        gl.uniform3f(colorUniform, color4[0], color4[1], color4[2]);
+        gl.drawArrays(gl.LINES, 36, 2);
+    }
+
+    // Desenha linha vertical
+    for(let i=-9; i<=9; i++){
+        linha[0] = i;
+        linha[2] = -7;
+        modelLinha = mat4.fromTranslation([],linha);
+        gl.uniformMatrix4fv(modelUniform, false, modelLinha);
+        gl.uniform3f(colorUniform, color4[0], color4[1], color4[2]);
+        gl.drawArrays(gl.LINES, 38, 2);
+    }
+    */
+
+    return;
+}
+
+function colisaoCenario(){
+    // Borda direita
+    if(cobra[0].position.x >= 6){
+        return true;
+    }
+
+    // Borda esquerda
+    if(cobra[0].position.x <= -6){
+        return true;
+    }
+
+    // Borda Inferior
+    if(cobra[0].position.z >= 6){
+        return true;
+    }
+
+    // Borda Superior
+    if(cobra[0].position.z <= -6){
+        return true;
+    }
+
+    return false;
+
 }
 
 main();
